5c356d305d1588ba91ecb1c1ef8b1d27911f5fc7
Ngalim Siregar <ngalim.siregar@gmail.com>
2018-07-02 18:08:50 +0900

    validate minimum comment length and remove max char limit

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -3,10 +3,15 @@ from wtforms import TextAreaField
 from wtforms import SubmitField
 from wtforms.validators import DataRequired
 from wtforms.validators import Length
+from wtforms.validators import ValidationError
 
 
 class CommentForm(FlaskForm):
     messages = TextAreaField(
-        "comment", validators=[DataRequired(), Length(min=10, max=140)]
+        "comment", validators=[DataRequired(), Length(min=10)]
     )
     submit = SubmitField("reply")
+
+    def validate_messages(self, messages):
+        if len(messages.data.strip()) < 10 :
+            raise ValidationError("minimum comment require 10 characters")
