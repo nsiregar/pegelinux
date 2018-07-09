eb41b667d4cc5de1586d08a82339e947cb533580
Ngalim Siregar <ngalim.siregar@gmail.com>
2018-07-10 04:39:07 +0900

    add command promote and demote user instead default value as admin

--- scripts/user_script.py
+++ scripts/user_script.py
@@ -14,10 +14,29 @@ user_command = AppGroup("user")
 def create_user(username, email, password):
     user = User(username=username, email=email)
     user.set_password(password)
-    user.role = "admin"
+    user.role = "jelata"
     db.session.add(user)
     db.session.commit()
     print("User {} created".format(username))
+
+
+@user_command.command("promote")
+@click.argument("username")
+@click.option("--role", default="momod", help="user role")
+def promote_user(username):
+    user = User.query.filter_by(username=username).first()
+    user.role = role
+    db.session.commit()
+    print("User {} promoted to {}".format(username, role))
+
+
+@user_command.command("demote")
+@click.argument("username")
+def demote_user(username):
+    user = User.query.filter_by(username=username).first()
+    user.role = "jelata"
+    db.session.commit()
+    print("User {} demoted".format(username))
 
 
 app.cli.add_command(user_command)
