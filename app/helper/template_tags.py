eb05bef69eee16769d857d9540da2a382dbb8344
Ngalim Siregar <ngalim.siregar@gmail.com>
2018-07-09 17:30:18 +0900

    fix comment order in replies

--- app/helper/template_tags.py
+++ app/helper/template_tags.py
@@ -3,7 +3,7 @@ from app.models.comment import Comment
 
 
 def get_children_comments(comment):
-    comments = Comment.query.filter_by(parent_id=comment.id)
+    comments = Comment.query.filter_by(parent_id=comment.id).order_by(Comment.created_at)
     return comments
 
 
