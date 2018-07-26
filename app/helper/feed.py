cff855515293619a06b08f78dbfca340ab577ccb
Ngalim Siregar <ngalim.siregar@gmail.com>
2018-07-26 23:50:54 +0900

    update feed client to handle http status

--- app/helper/feed.py
+++ app/helper/feed.py
@@ -13,6 +13,12 @@ def getFeed():
     urls = Feed.query.filter_by(approved=APPROVED).all()
     for url in urls:
         feed = parseFeed(url.rss)
+        if feed.status == 301:
+            return 'feed redirected please update to new feed'
+        if feed.status == 410:
+            return 'feed is gone'
+        if feed.status == 404:
+            return 'feed not found'
         for item in feed.entries:
             pubtime = datetime.datetime(*(item.published_parsed[0:7]))
             record = Post.query.filter_by(url=item.link).first()
