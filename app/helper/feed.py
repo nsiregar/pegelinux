07ea111b006308a0e2b4639d0d4bbd62c8328eab
Ngalim Siregar <ngalim.siregar@gmail.com>
2018-07-27 10:04:30 +0900

    parse only http ok status code

--- app/helper/feed.py
+++ app/helper/feed.py
@@ -13,23 +13,20 @@ def getFeed():
     urls = Feed.query.filter_by(approved=APPROVED).all()
     for url in urls:
         feed = parseFeed(url.rss)
-        if feed.status == 301:
-            return 'feed redirected please update to new feed'
-        if feed.status == 410:
-            return 'feed is gone'
-        if feed.status == 404:
-            return 'feed not found'
-        for item in feed.entries:
-            pubtime = datetime.datetime(*(item.published_parsed[0:7]))
-            record = Post.query.filter_by(url=item.link).first()
-            if record is None:
-                post = Post(
-                    title=item.title,
-                    url=item.link,
-                    date=pubtime,
-                    owner=url.owner,
-                    domain=url.html,
-                )
-                db.session.add(post)
-                db.session.commit()
-                print("News added from {}".format(item.link))
+        if feed.status == 200:
+            for item in feed.entries:
+                pubtime = datetime.datetime(*(item.published_parsed[0:7]))
+                record = Post.query.filter_by(url=item.link).first()
+                if record is None:
+                    post = Post(
+                        title=item.title,
+                        url=item.link,
+                        date=pubtime,
+                        owner=url.owner,
+                        domain=url.html,
+                    )
+                    db.session.add(post)
+                    db.session.commit()
+                    print("News added from {}".format(item.link))
+        else:
+            print("Status code {} for {}".format(feed.status, url.rss))
