b489d0acb91fbdd274932ab9d8293565e2c7f179
Rezha Julio <rezhajulio@users.noreply.github.com>
2019-05-23 19:08:42 +0700

    Update user_script.py
    Add job command to get feed

--- scripts/user_script.py
+++ scripts/user_script.py
@@ -3,8 +3,11 @@ from app import app
 from app import db
 from flask.cli import AppGroup
 from app.models.user import User
+from app.helper.feed import getFeed
+
 
 user_command = AppGroup("user")
+job_command = AppGroup("job")
 
 
 @user_command.command("create")
@@ -37,6 +40,12 @@ def demote_user(username):
     user.role = "jelata"
     db.session.commit()
     print("User {} demoted".format(username))
+    
+ 
+@job_command.command("feed")
+def get_feed():
+    getFeed()
 
 
 app.cli.add_command(user_command)
+app.cli.add_command(job_command)
