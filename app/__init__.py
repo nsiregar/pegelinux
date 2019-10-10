ff9716bca99286725d29c9dd7d73aa4087b3fb53
Ngalim Siregar <ngalim.siregar@gmail.com>
2019-10-10 22:24:06 +0700

    Add error monitoring with sentry

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
 import os
+import sentry_sdk
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_login import LoginManager
 from flask_misaka import Misaka
+from sentry_sdk.integrations.flask import FlaskIntegration
 
+sentry_dsn = os.environ.get("SENTRY_DSN")
+sentry_id = os.environ.get("SENTRY_ID")
+sentry_url = f"https://{ sentry_dsn }@sentry.io/{ sentry_id }"
+
+sentry_sdk.init(
+        dsn=sentry_url,
+        integrations=[FlaskIntegration()]
+        )
+
+app_config = os.environ.get("APP_CONFIG")
 app = Flask(__name__, static_folder="assets", template_folder="views")
-app.config.from_object(os.environ.get("APP_CONFIG"))
+app.config.from_object(app_config)
 moment = Moment(app)
 login = LoginManager(app)
 login.login_view = "auth.login"
