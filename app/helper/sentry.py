2f5128cf210f388cbf42c40e7b8383f253715a00
Ngalim Siregar <ngalim.siregar@gmail.com>
2019-10-13 11:05:41 +0700

    Refactor sentry initialization

---
+++ app/helper/sentry.py
@@ -0,0 +1,16 @@
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from app import app
+
+
+def init():
+    sentry_dsn = app.config.get("SENTRY_DSN")
+    sentry_id = app.config.get("SENTRY_ID")
+    if (sentry_dsn and sentry_id) is None:
+        return
+
+    sentry_url = f"https://{ sentry_dsn }@sentry.io/{ sentry_id }"
+    sentry_sdk.init(
+        dsn=sentry_url,
+        integrations=[FlaskIntegration()]
+        )
