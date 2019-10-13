0fd74e3d74083bce917a9e05815a831dc448d12a
Ngalim Siregar <ngalim.siregar@gmail.com>
2019-10-13 11:05:41 +0700

    Refactor sentry initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,24 @@
 import os
-import sentry_sdk
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_login import LoginManager
 from flask_misaka import Misaka
-from sentry_sdk.integrations.flask import FlaskIntegration
-
-sentry_dsn = os.environ.get("SENTRY_DSN")
-sentry_id = os.environ.get("SENTRY_ID")
-sentry_url = f"https://{ sentry_dsn }@sentry.io/{ sentry_id }"
-
-sentry_sdk.init(
-        dsn=sentry_url,
-        integrations=[FlaskIntegration()]
-        )
+from app.helper import sentry
 
 app_config = os.environ.get("APP_CONFIG")
 app = Flask(__name__, static_folder="assets", template_folder="views")
 app.config.from_object(app_config)
+
 moment = Moment(app)
 login = LoginManager(app)
 login.login_view = "auth.login"
 Misaka(app, no_intra_emphasis=True, escape=True, autolink=True, math=True)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+sentry.init()
 
 from app.controllers import routes
 from app.controllers import scheduler
